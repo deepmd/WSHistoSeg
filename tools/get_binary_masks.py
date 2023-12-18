dc5327a50f9131066223e5d9f167d62acc3cdc53
reza_safdari <reza_safdari@outlook.com>
2023-12-18 20:13:49 +0330

    generating binary masks based on different thresholds and plotting false positive and false negative regions.

--- tools/get_binary_masks.py
+++ tools/get_binary_masks.py
@@ -1,8 +1,9 @@
 import os
 import argparse
 from glob import glob
-import numpy as np
+from collections import OrderedDict
 
+import numpy as np
 import cv2
 from PIL import Image, ImageDraw, ImageFont
 from torchcam.utils import overlay_mask
@@ -27,18 +28,19 @@ def get_dice_metric(cam, mask, tau=None):
     sample_evaluator = MaskEvaluation(cam_curve_interval=0.001, best_valid_tau=tau)
     sample_evaluator.accumulate(cam, mask)
     sample_evaluator.compute()
-    return (float(sample_evaluator.perf_gist['Dice foreground']),
-            float(sample_evaluator.perf_gist['Dice background']),
-            float(sample_evaluator.perf_gist['mdice_best']))
+    return (sample_evaluator.perf_gist['Dice foreground'],
+            sample_evaluator.perf_gist['Dice background'],
+            sample_evaluator.perf_gist['mdice_best'])
 
 
 def get_binary_masks(cam, thresholds, image_name):
     image_name, ext = os.path.splitext(image_name)
-    return {f'{image_name}_th_{thresh}': (cam > thresh).astype(np.uint8) * 255
-            for thresh in thresholds}
+    return OrderedDict({f'{image_name}_th{idx}_{thresh}': (cam > thresh).astype(np.uint8) * 255
+                        for idx, thresh in enumerate(thresholds)})
 
 
-def get_overlay_mask(image, bi_mask, colormap, alpha, additional_height, font_size, text_color, text=None):
+def get_overlay_mask(image, bi_mask, colormap='jet', alpha=0.6,
+                     additional_height=30, font_size=10, text_color='black', text=None):
     if np.max(bi_mask) == 255:
         bi_mask = (bi_mask / 255).astype(np.float32)
     bi_mask_on_image, _ = overlay_mask(to_pil_image(image),
@@ -61,6 +63,33 @@ def get_overlay_mask(image, bi_mask, colormap, alpha, additional_height, font_si
     text_position = ((width - text_size[0]) // 2, 0)
     draw.text(text_position, text, fill=text_color, font=font)
     return final_image
+
+
+def save_fn_fp_masks(image, gt_mask, best_thresh_mask, save_path):
+    save_path = os.path.join(save_path, 'fp_fn_masks')
+    os.makedirs(save_path, exist_ok=True)
+
+    mask_name, bi_mask = best_thresh_mask
+    if bi_mask.max() == 255:
+        best_thresh_mask = (bi_mask / 255).astype(np.uint8)
+    # Create false negative mask and false positive mask
+    fn_mask = (gt_mask & (1 - best_thresh_mask)) * 255
+    fp_mask = ((1 - gt_mask) & best_thresh_mask) * 255
+    # Create red mask for false negatives and blue mask for fave positives (BGR)
+    fn_red_mask = np.stack([np.zeros_like(fn_mask)] * 2 + [fn_mask], axis=-1)
+    fp_blue_mask = np.stack([fp_mask] + [np.zeros_like(fp_mask)] * 2, axis=-1)
+    fn_fp_mask = np.stack([fp_mask] + [np.zeros_like(fp_mask)] + [fn_mask], axis=-1)
+    # Overlay false negative red mask on the image
+    mask_on_image, _ = overlay_mask(to_pil_image(image), to_pil_image(gt_mask.astype(np.float32)), alpha=0.6)
+    fn_mask_on_image = cv2.addWeighted(np.asarray(mask_on_image).astype(np.uint8), 0.4, fn_red_mask, 1, 0)
+    fp_mask_on_image = cv2.addWeighted(np.asarray(mask_on_image).astype(np.uint8), 0.4, fp_blue_mask, 1, 0)
+    fp_fn_mask_on_image = cv2.addWeighted(np.asarray(mask_on_image).astype(np.uint8), 0.4, fn_fp_mask, 1, 0)
+
+    # Save the results
+    mask_on_image.save(os.path.join(save_path, f'{mask_name}_gt_mask.png'))
+    cv2.imwrite(os.path.join(save_path, f'{mask_name}_fn_mask.png'), fn_mask_on_image)
+    cv2.imwrite(os.path.join(save_path, f'{mask_name}_fp_mask.png'), fp_mask_on_image)
+    cv2.imwrite(os.path.join(save_path, f'{mask_name}_fp_fn_mask.png'), fp_fn_mask_on_image)
 
 
 def save_masks(image, bi_mask_dict, dice_list, save_dir, mask_on_image,
@@ -96,14 +125,15 @@ def main(cfg):
 
         image = cv2.imread(image_path, cv2.COLOR_BGR2RGB)
         image = cv2.resize(image, (cfg.resize_size, cfg.resize_size), interpolation=cv2.INTER_LINEAR)
-        # height, width, _ = image.shape
 
-        cam = torch.Tensor(np.load(cam_path))
-        cam = F.interpolate(cam.unsqueeze(0).unsqueeze(0),
-                            (cfg.resize_size, cfg.resize_size),
-                            mode='bicubic',
-                            align_corners=False)
-        cam = torch.sigmoid(cam.squeeze()).detach().cpu().numpy().astype(float)
+        cam = np.load(cam_path)
+        if cfg.method != 'negev':
+            cam = torch.Tensor(cam).unsqueeze(0).unsqueeze(0)
+            cam = F.interpolate(cam,
+                                (cfg.resize_size, cfg.resize_size),
+                                mode='bicubic',
+                                align_corners=False).squeeze()
+            cam = torch.sigmoid(cam).numpy().astype(float)
 
         gt_mask = cv2.imread(mask_path, cv2.IMREAD_GRAYSCALE) > 0.5
         gt_mask = cv2.resize(gt_mask.astype(np.uint8),
@@ -117,6 +147,7 @@ def main(cfg):
         save_masks(image, binary_mask_dict, dice_list, binary_mask_dir,
                    cfg.binary_mask_on_image, cfg.colormap, cfg.alpha,
                    cfg.additional_height, cfg.font_size, cfg.text_color)
+        save_fn_fp_masks(image, gt_mask, binary_mask_dict.popitem(last=True), binary_mask_dir)
         print(f'{idx + 1}/{len(cam_paths)}: {image_name}')
 
 
@@ -124,9 +155,9 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='')
     parser.add_argument('--cam_dir', type=str)
     parser.add_argument('--image_dir', type=str)
-    parser.add_argument('--binary_mask_dir', type=str, default='binary_masks1',
+    parser.add_argument('--binary_mask_dir', type=str, default='binary_masks',
                         help='The path where the generated binary masks will be saved.')
-    parser.add_argument('--method', type=str, default='ours', choices=['ours', 'gradcam', 'negev'])
+    parser.add_argument('--method', type=str, default='negev', choices=['ours', 'negev'])
     parser.add_argument('--split', type=str, default='test', choices=['train', 'test', 'train+test'])
     parser.add_argument('--thresholds', type=str, default='')
     parser.add_argument('--binary_mask_on_image', action='store_true',
